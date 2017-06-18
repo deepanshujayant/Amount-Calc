c964c73b5f9004f1886adec1000a7edfdd35102b
deepu97 <deepanshujayant@gmail.com>
2017-06-18 14:27:24 +0530

    Files

---
+++ src/pricecalc/amt.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pricecalc;
+
+/**
+ *
+ * @author Deepanshu
+ */
+public class amt {
+    float num1;
+    int num2;
+    public float Xmul(float n1, int n2) {
+        float xprod;
+        xprod = n1*n2;
+        return xprod;
+    }
+    
+    
+}
